5b07df6e52a2b515d9f891cce5ad3d6345543f87
quigl <quigleytan@gmail.com>
2026-02-28 10:50:11 -0500

    Added sql scheme

--- back-end/schema.sql
+++ back-end/schema.sql
@@ -4,5 +4,19 @@ CREATE TABLE IF NOT EXISTS users (
     password_hash TEXT NOT NULL,
     level INTEGER DEFAULT 1,
     gold INTEGER DEFAULT 0,
+    diamonds INTEGER DEFAULT 0,
+    silver INTEGER DEFAULT 0,
+    iron INTEGER DEFAULT 0,
+    copper INTEGER DEFAULT 0,
     plaid_access_token TEXT
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    transaction_id TEXT UNIQUE,
+    name TEXT,
+    amount REAL,
+    date TEXT,
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
