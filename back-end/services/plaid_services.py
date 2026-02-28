f187be4f1badbedd97b9ae88974ae1d8df5f763f
Blas Urbano <79434789+Blas-U@users.noreply.github.com>
2026-02-28 12:03:06 -0500

    update

--- back-end/services/plaid_services.py
+++ back-end/services/plaid_services.py
@@ -3,23 +3,11 @@ from plaid.api import plaid_api
 from plaid.model.transactions_sync_request import TransactionsSyncRequest
 from plaid.model.sandbox_public_token_create_request import SandboxPublicTokenCreateRequest
 from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
+from plaid.model.products import Products 
 
 client_id = '69a3011352ca8e000ebb46fa'
 secret = 'f6e227559ebed7d66430b477b5a0ac59'
 
-def get_sandbox_access_token():
-    sandbox_req = SandboxPublicTokenCreateRequest(
-        institution_id='ins_109508', # Chase Bank
-        initial_products=['transactions']
-    )
-    public_token = client.sandbox_public_token_create(sandbox_req).public_token
-    exchange_req = ItemPublicTokenExchangeRequest(public_token=public_token)
-    access_token = client.item_public_token_exchange(exchange_req).access_token
-    return access_token
-
-# Available environments are
-# 'Production'
-# 'Sandbox'
 configuration = plaid.Configuration(
     host=plaid.Environment.Sandbox,
     api_key={
@@ -27,9 +15,22 @@ configuration = plaid.Configuration(
         'secret': secret,
     }
 )
-
 api_client = plaid.ApiClient(configuration)
 client = plaid_api.PlaidApi(api_client)
+
+def get_sandbox_access_token():
+    sandbox_req = SandboxPublicTokenCreateRequest(
+        institution_id='ins_109508', 
+        initial_products=[Products('transactions')]
+    )
+    
+    # Using dictionary access for the response is safer in the newer Plaid SDK
+    public_token = client.sandbox_public_token_create(sandbox_req)['public_token']
+    
+    exchange_req = ItemPublicTokenExchangeRequest(public_token=public_token)
+    access_token = client.item_public_token_exchange(exchange_req)['access_token']
+    
+    return access_token
 
 access_token = get_sandbox_access_token()
 
@@ -48,3 +49,5 @@ while (response['has_more']):
     )
     response = client.transactions_sync(request)
     transactions += response['added']
+
+print(f"Success! Retrieved {len(transactions)} transactions.")
